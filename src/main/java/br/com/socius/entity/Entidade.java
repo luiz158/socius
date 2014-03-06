f4ed8ed3f5f88203231292e261dc7071004a0fd6
Luciano Borges <lucianosantosborges@gmail.com>
2014-03-05 23:47:03 -0400

    Criada entidade Entidade.

---
+++ src/main/java/br/com/socius/entity/Entidade.java
@@ -0,0 +1,73 @@
+package br.com.socius.entity;
+
+import java.io.Serializable;
+import java.net.URL;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.validator.constraints.NotBlank;
+
+@Entity
+@Table(name = "entidade")
+public class Entidade implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotBlank
+	private String nome;
+
+	private String sigla;
+
+	private URL site;
+
+	private String cnpj;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public URL getSite() {
+		return site;
+	}
+
+	public void setSite(URL site) {
+		this.site = site;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+}
